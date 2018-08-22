9b18b7d1cad7ad1bf2d2f82a6555b88721a8c72d
Emily Wilder <emilywilder.earth@gmail.com>
2018-08-22 15:06:58 -0400

    fix typo

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,11 +1,11 @@
 function start() {
-    console.log("Request handler 'start' was called.")
-    reutrn "Hello Start!"
+    console.log("Request handler 'start' was called.");
+    return "Hello Start!"
 }
 
 function upload() {
-    console.log("Request handler 'upload' was called.")
-    reutrn "Hello Upload!"
+    console.log("Request handler 'upload' was called.");
+    return "Hello Upload!"
 }
 
 exports.start = start
