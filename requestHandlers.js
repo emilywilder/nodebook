5df164f418e02738e2a32958be5f691fb8e771d6
Emily Wilder <emilywilder.earth@gmail.com>
2018-08-22 15:05:12 -0400

    add request handling THE WRONG WAY

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,11 @@
 function start() {
     console.log("Request handler 'start' was called.")
+    reutrn "Hello Start!"
 }
 
 function upload() {
     console.log("Request handler 'upload' was called.")
+    reutrn "Hello Upload!"
 }
 
 exports.start = start
